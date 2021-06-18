11c892d9dbb921043c58c81ffe313e97a878759c
Pier Fiedorowicz <pierfied@email.arizona.edu>
2021-06-17 21:32:40 -0700

    Fixed some bugs in the sampler class.

--- nnacc/sampler.py
+++ nnacc/sampler.py
@@ -8,17 +8,17 @@ class Sampler:
         self.transform = transform
         self.device=device
 
-        if self.x0 is None:
+        if x0 is None:
             self.x0 = torch.randn(self.nparams, device=self.device)
         else:
-            self.x0 = x0.to(torch.float32, device=device)
+            self.x0 = x0.to(dtype=torch.float32, device=device)
 
         self.nparams = len(self.x0)
 
-        if self.m is None:
+        if m is None:
             self.m = torch.ones(self.nparams, device=self.device)
         else:
-            self.m = m.to(torch.float32, device=device)
+            self.m = m.to(dtype=torch.float32, device=device)
 
     def calc_hess_mass_mat(self, nsteps=1000, eps=1e-4, resamp_x0=True):
         x = self.x0.clone().requires_grad_()
@@ -27,10 +27,11 @@ class Sampler:
         for i in pbar:
             lnp = self.lnp(x)
             grad = torch.autograd.grad(lnp, x)[0]
-            x += grad * eps
-            pbar.set_description('log-prob: {}'.format(x))
+            x = x + grad * eps
+            pbar.set_description('log-prob: {}'.format(lnp))
 
         hess = []
+        lnp = self.lnp(x)
         grad = torch.autograd.grad(lnp, x, create_graph=True)[0]
         for i in range(self.nparams):
             hess.append(torch.autograd.grad(grad[i], x, retain_graph=True)[0])
@@ -42,15 +43,16 @@ class Sampler:
         self.u = u
         self.m = m
         self.orig_lnp = self.lnp
-        self.lnp = lambda x: self.orig_lnp(self.u @ x)
+        self.xmap = x.detach().clone()
+        self.lnp = lambda x: self.orig_lnp(self.xmap + self.u @ x)
         if self.transform is None:
-            self.transform = lambda x: self.u @ x
+            self.transform = lambda x: self.xmap + self.u @ x
         else:
             self.orig_transform = self.transform
-            self.transform = lambda x: self.orig_transform(self.u @ x)
+            self.transform = lambda x: self.orig_transform(self.xmap + self.u @ x)
 
         if resamp_x0:
-            self.x0 = x + torch.randn(self.nparams, device=self.device) * torch.sqrt(s)
+            self.x0 = torch.randn(self.nparams, device=self.device) * torch.sqrt(s)
 
     def sample(self, nburn, burn_steps, burn_eps, nsamp, samp_steps, samp_eps):
         hmc = HMCSampler(self.lnp, self.x0, self.m, self.transform, device=self.device)
